7699a2797db6a12811d65d13e504ec5ac7a394d2
stdcout42 <sammm.sh@gmail.com>
2018-12-02 16:05:53 +0100

    init chessboard pieces, add pieces.js

--- ChessWD/public/javascripts/main2.js
+++ ChessWD/public/javascripts/main2.js
@@ -1,20 +1,46 @@
 function init () {
-  createEmptyBoard()
+  createEmptyBoardUI()
+  initPieces()
 }
 
-function createEmptyBoard () {
+function createEmptyBoardUI () {
   var coords = ''
   for (var r = 0; r < 8; r++) {
     var col = ''
     for (var c = 0; c < 8; c++) {
-      col +=
-        "<td data-pos='" + String.fromCharCode(65 + c) + (8 - r) + "'></td>"
+      coords = String.fromCharCode(65 + c) + (8 - r)
+      col += "<td id='" + coords + "'></td>"
     }
     $('#chessboard').append('<tr>' + col + '</tr>')
   }
   $('#chessboard tr td').click(function () {
-    var cellpos = $(this).attr('data-pos')
-    console.log('cell' + cellpos)
+    var cellpos = $(this).attr('id')
+    console.log('cell:' + cellpos + ' clicked')
   })
 }
+
+function initPieces () {
+  for (var r = 0; r < 8; r++) {
+    var col = ''
+    for (var c = 0; c < 8; c++) {
+      coords = String.fromCharCode(65 + c) + (8 - r)
+      PIECES.forEach(function (piece) {
+        piece.startingPos.forEach(function (pos) {
+          if (pos == coords) {
+            $('#' + coords).append(
+              $(
+                "<img class='chesspiece' id=" +
+                  piece.id +
+                  ' src=' +
+                  piece.icon +
+                  '>'
+              )
+            )
+          }
+        })
+      })
+    }
+  }
+}
+
 $(document).ready(init)
---
+++ ChessWD/public/javascripts/pieces.js
@@ -0,0 +1,83 @@
+var whitePawn = {
+  id: 'wP',
+  icon: 'images/gamePieces/pawn-W.png',
+  startingPos: ['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2']
+}
+
+// note: white knight = wN, wK = white king
+var whiteKnight = {
+  id: 'wN',
+  icon: 'images/gamePieces/knight-W.png',
+  startingPos: ['B1', 'G1']
+}
+var whiteBishop = {
+  id: 'wB',
+  icon: 'images/gamePieces/bishop-W.png',
+  startingPos: ['C1', 'F1']
+}
+
+var whiteQueen = {
+  id: 'wQ',
+  icon: 'images/gamePieces/queen-W.png',
+  startingPos: ['D1']
+}
+
+var whiteKing = {
+  id: 'wK',
+  icon: 'images/gamePieces/king-W.png',
+  startingPos: ['E1']
+}
+
+var whiteRook = {
+  id: 'wR',
+  icon: 'images/gamePieces/rook-W.png',
+  startingPos: ['A1', 'H1']
+}
+var blackPawn = {
+  id: 'bP',
+  icon: 'images/gamePieces/pawn-B.png',
+  startingPos: ['A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7']
+}
+var blackKnight = {
+  id: 'bN',
+  icon: 'images/gamePieces/knight-B.png',
+  startingPos: ['B8', 'G8']
+}
+var blackBishop = {
+  id: 'bB',
+  icon: 'images/gamePieces/bishop-B.png',
+  startingPos: ['C8', 'F8']
+}
+
+var blackQueen = {
+  id: 'bQ',
+  icon: 'images/gamePieces/queen-B.png',
+  startingPos: ['D8']
+}
+
+var blackKing = {
+  id: 'bK',
+  icon: 'images/gamePieces/king-B.png',
+  startingPos: ['E8']
+}
+
+var blackRook = {
+  id: 'bR',
+  icon: 'images/gamePieces/rook-B.png',
+  startingPos: ['A8', 'H8']
+}
+
+var PIECES = [
+  whitePawn,
+  whiteKnight,
+  whiteBishop,
+  whiteQueen,
+  whiteKing,
+  whiteRook,
+  blackPawn,
+  blackKnight,
+  blackBishop,
+  blackQueen,
+  blackKing,
+  blackRook
+]
