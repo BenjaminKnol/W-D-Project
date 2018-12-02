1404e52f83ba68eebcf2af344ec6da55b3cb8841
stdcout42 <sammm.sh@gmail.com>
2018-12-02 13:17:15 +0100

    Add coords instead of cellnr

--- ChessWD/public/javascripts/main2.js
+++ ChessWD/public/javascripts/main2.js
@@ -1,10 +1,14 @@
 function init () {
-  var cellnr = 0
+  createEmptyBoard()
+}
+
+function createEmptyBoard () {
+  var coords = ''
   for (var r = 0; r < 8; r++) {
     var col = ''
     for (var c = 0; c < 8; c++) {
-      col += "<td data-pos='" + cellnr + "'></td>"
-      cellnr++
+      col +=
+        "<td data-pos='" + String.fromCharCode(65 + c) + (8 - r) + "'></td>"
     }
     $('#chessboard').append('<tr>' + col + '</tr>')
   }
@@ -13,4 +17,4 @@ function init () {
     console.log('cell' + cellpos)
   })
 }
-$(document).ready(init);
+$(document).ready(init)
